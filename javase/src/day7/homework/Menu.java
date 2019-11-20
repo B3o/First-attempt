d068a8d2b193b5b9d25139434912eb616a6f7216
Bmo <84262602@qq.com>
2019-11-20 09:31:56 +0800

    11.20

--- javase/src/day7/homework/Menu.java
+++ javase/src/day7/homework/Menu.java
@@ -1,5 +1,7 @@
 package day7.homework;
 
+import org.junit.Test;
+
 import java.io.*;
 import java.util.*;
 
@@ -9,10 +11,10 @@ import java.util.*;
  */
 public class Menu implements Serializable {
     HashMap<String, User> users = new HashMap<>();
-    Scanner sc = new Scanner(System.in);
     User currentUser;
 
     public void register() {
+        Scanner sc = new Scanner(System.in);
         String username, password;
         int role;
         System.out.println("(1.超级管理员 2.管理员  3.普通用户  4.游客)");
@@ -24,10 +26,15 @@ public class Menu implements Serializable {
             System.err.println("该用户名已存在，请重新注册");
             return;
         }
-        System.out.print("请设置你的密码");
+        System.out.print("请设置你的密码：");
         password = sc.next();
         User user = new User(role, username, password);
         users.put(username, user);
+        try {
+            storeInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public void login() {
         String username, password;
@@ -43,66 +50,114 @@ public class Menu implements Serializable {
         if (password.equals(users.get(username).getPassword())) {
             System.out.println("登陆成功");
             currentUser = users.get(username);
-            //打印菜单方法
+            printLoginMenu();
         } else {
             System.err.println("密码错误，请重试");
-
         }
     }
 
     public void printLoginMenu() {
-        if (currentUser.getRole() == 1 || currentUser.getRole() == 2){
-            System.out.println("\t\t1.展示用户列表");
-            System.out.println("\t\t2.删除用户");
-            System.out.println("\t\t3.修改用户");
-            System.out.println("\t\t0.退出登陆");
-        } else {
-            System.out.println("\t\t1.展示用户列表");
-            System.out.println("\t\t2.修改用户");
-            System.out.println("\t\t0.退出登陆");
-        }
-        System.out.println("请输入你下一步的操作：");
-        int choice = sc.nextInt();
-        if (currentUser.getRole() == 1 || currentUser.getRole() == 2) {
-            switch (choice) {
-                case 1:
-                    this.listUser();
-                    break;
-                case 2:
-                    //删除用户
-                    break;
-                case 3:
-                    //修改用户
-                    break;
-                default:
-                    return;
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            if (currentUser.getRole() == 1 || currentUser.getRole() == 2){
+                System.out.println("\t\t1.展示用户列表");
+                System.out.println("\t\t2.删除用户");
+                System.out.println("\t\t3.修改密码");
+                System.out.println("\t\t0.退出登陆");
+            } else {
+                System.out.println("\t\t1.展示用户列表");
+                System.out.println("\t\t2.修改密码");
+                System.out.println("\t\t0.退出登陆");
             }
-        } else {
-            switch (choice) {
-                case 1:
-                    this.listUser();
-                    break;
-                case 2:
-                    //修改用户
-                    break;
-                default:
-                    return;
+            System.out.println("请输入你下一步的操作：");
+            choice = sc.nextInt();
+            if (currentUser.getRole() == 1 || currentUser.getRole() == 2) {
+                switch (choice) {
+                    case 1:
+                        listUser();
+                        break;
+                    case 2:
+                        deleteUser();
+                        break;
+                    case 3:
+                        modifyUser();
+                        break;
+                    default:
+                        currentUser = null;
+                        break;
+                }
+            } else {
+                switch (choice) {
+                    case 1:
+                        listUser();
+                        break;
+                    case 2:
+                        modifyUser();
+                        break;
+                    default:
+                        currentUser = null;
+                        break;
+                }
             }
-        }
-
+        } while (choice != 0);
     }
     public void listUser() {
-        User currentUser;
+        Scanner sc = new Scanner(System.in);
+        User tempUser;
         Iterator usersLit = users.entrySet().iterator();
         System.out.println("用户名-----------角色");
         while (usersLit.hasNext()) {
             Map.Entry<String, User> entry = (Map.Entry<String, User>) usersLit.next();
-            currentUser = entry.getValue();
-            System.out.println(currentUser.getUsername() + "-----------" + currentUser.getRoleStr());
+            tempUser = entry.getValue();
+            System.out.println(tempUser.getUsername() + "-----------" + tempUser.getRoleStr());
         }
     }
     public void modifyUser() {
-        System.out.println("个人练习");
+        Scanner sc = new Scanner(System.in);
+        String password;
+        System.out.println("请问你要把密码修改为什么呢？");
+        password = sc.next();
+        currentUser.setPassword(password);
+        users.remove(currentUser.getUsername());
+        users.put(currentUser.getUsername(), currentUser);
+        try {
+            storeInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("修改成功！");
+    }
+    public void deleteUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===========目前所有用户：");
+        this.listUser();
+        System.out.print("请输入你要删除的用户的用户名：");
+        String username = sc.next();
+        if (username.equals(currentUser.getUsername())) {
+            System.err.println("你不能删除自己！");
+            return;
+        }
+        users.remove(username);
+        try {
+            storeInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("删除成功");
+    }
+
+    public void storeInfo() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        File file = new File("F:\\practiceIO\\userInfo.dll");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileOutputStream fos = new FileOutputStream(file);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(this);
+        oos.close();
+        fos.close();
     }
 
 
--- javase/src/day7/homework/Start.java
+++ javase/src/day7/homework/Start.java
@@ -1,26 +1,54 @@
 package day7.homework;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * @author Bmo
  * @date 2019/11/19 19:11
  */
 public class Start {
-    Menu menu = new Menu();
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Menu menu = null;
+        try {
+            menu = getStoreInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (menu == null) {
+            menu = new Menu();
+        }
+        int choice;
+        do {
+            System.out.println("欢迎进入人员管理系统");
+            System.out.println("1.注册     2.登陆   0.退出");
+            System.out.print("请选择你下一步操作：");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    menu.register();
+                    break;
+                case 2:
+                    menu.login();
+                    break;
+            }
+        } while (choice != 0);
     }
-    public void storeInfo() throws IOException {
+    public static Menu getStoreInfo() throws IOException, ClassNotFoundException {
         File file = new File("F:\\practiceIO\\userInfo.dll");
         if (!file.exists()) {
-            file.createNewFile();
+            System.err.println("序列化文件不存在，反序列化失败！");
+            return null;
         }
-        FileOutputStream fos = new FileOutputStream(file);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(menu);
+        FileInputStream fis = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Menu temp = (Menu)ois.readObject();
+        ois.close();
+        fis.close();
+        return temp;
     }
 }
